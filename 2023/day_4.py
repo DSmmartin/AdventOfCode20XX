efe4349f9db303a3a7e57e12d7fa05b7a9d7818e
DSmmartin <mjmm_manuel@hotmail.com>
2023-12-07 19:02:09 +0000

    add to 2023 day 4, puzzle 2

--- 2023/day_4.py
+++ 2023/day_4.py
@@ -80,3 +80,89 @@ for card_result in input_cards_results:
     total_worth += (number_my_win_tickets > 0) * worth_card_result
 
 print(total_worth)
+
+"""
+--- Part Two ---
+Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
+
+There's no such thing as "points". Instead, scratchcards only cause you to win more scratchcards equal to the number of winning numbers you have.
+
+Specifically, you win copies of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+
+Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied. So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you to win any more cards. (Cards will never make you copy a card past the end of the table.)
+
+This time, the above example goes differently:
+
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+Your copy of card 2 also wins one copy each of cards 3 and 4.
+Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+Once all of the originals and copies have been processed, you end up with 1 instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of card 4, 14 instances of card 5, and 1 instance of card 6. In total, this example pile of scratchcards causes you to ultimately have 30 scratchcards!
+
+Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
+"""
+
+def parse_content_card_result(content_card_result: str):
+    content_card_result = " ".join(content_card_result.split())
+    _, card_id_in_str = content_card_result.split(" ")
+    return int(card_id_in_str)
+
+class MyTicketsWorth:
+    def __init__(self, number_of_cards) -> None:
+        self._my_tickets_obtained = {(iter + 1): 1 for iter in list(range(number_of_cards))}
+        self._my_tickets_worth = {(iter + 1): 0 for iter in list(range(number_of_cards))}
+        self._number_of_cards = number_of_cards
+
+    def add_bonus_ticket(self, card_id, number_bonus):
+        list_of_extra_tickets = list(range((card_id + 1), (card_id + 1 + number_bonus)))
+        list_of_extra_tickets = list(filter(lambda id_ticket: id_ticket <= self._number_of_cards, list_of_extra_tickets))
+        _ = [self.add_extra_ticket_to_my_tickets(card_id, id__extra_ticket) for id__extra_ticket in list_of_extra_tickets]
+
+    def add_extra_ticket_to_my_tickets(self, current_winner_ticket, id_extra_ticket):
+        self._my_tickets_obtained[id_extra_ticket] += 1 * self._my_tickets_obtained[current_winner_ticket]
+
+    def set_worth_to_ticket(self, ticket_id, number_my_win_tickets):
+        exponent = number_my_win_tickets - 1
+        exponent_filtered = (exponent > 0) * exponent
+        worth_card_result = 2 ** (exponent_filtered)
+        self._my_tickets_worth[ticket_id] = self._my_tickets_obtained[ticket_id]
+
+    def get_total_worth(self):
+        return sum(list(self._my_tickets_worth.values()))
+
+
+input_cards_results = read_input_file(FILE_INPUT)
+
+# input_cards_results = [
+#     "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+#     "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+#     "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+#     "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+#     "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+#     "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+# ]
+
+my_tickets_worth = MyTicketsWorth(len(input_cards_results))
+
+
+for card_result in input_cards_results:
+    content_card_id, content_card_result = card_result.split(": ")
+    card_id = parse_content_card_result(content_card_id)
+    content_win_numbers, content_my_numbers = content_card_result.split(" | ")
+    win_numbers = obtain_numbers_from_content_results(content_win_numbers)
+    my_numbers = obtain_numbers_from_content_results(content_my_numbers)
+    my_win_numbers = obtain_my_win_numbers_given_the_results(win_numbers, my_numbers)
+    number_my_win_tickets = len(my_win_numbers)
+    my_tickets_worth.add_bonus_ticket(card_id, number_my_win_tickets)
+    my_tickets_worth.set_worth_to_ticket(card_id, number_my_win_tickets)
+
+print(my_tickets_worth.get_total_worth())
